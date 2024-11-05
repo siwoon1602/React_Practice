fe1042fe198b4dce0f98dd4ee69e24ea07766c9f
siwoon.yoon <siwoon@gmailcom>
2024-11-05 23:45:03 +0900

    :memo: useState 기능이용

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { React, useState } from "react";
 
 function App() {
+  const [title, setTitle] = useState([
+    "남자 코트 추천",
+    "강남 우동맛집",
+    "파이썬 독학",
+  ]);
+
+  const [like, setLike] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div className="black-nav">
+          <h4>React Blog</h4>
+        </div>
+        <div className="list">
+          <h4>
+            {title[0]}
+            <span
+              onClick={() => {
+                setLike(like + 1);
+              }}
+            >
+              👍
+            </span>
+
+            {like}
+            <button
+              onClick={() => {
+                let copy = [...title];
+                copy[0] = "여자 코트 추천";
+                setTitle(copy);
+              }}
+            >
+              Change!
+            </button>
+          </h4>
+
+          <p>2월 17일 발행</p>
+        </div>
+        <div className="list">
+          <h4>{title[1]}</h4>
+          <p>2월 17일 발행</p>
+        </div>
+        <div className="list">
+          <h4>{title[2]}</h4>
+          <p>2월 17일 발행</p>
+        </div>
+      </div>
+    </>
   );
 }
 
